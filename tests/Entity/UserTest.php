6ff6c3131e5a3c8286c31a415345f4574c192328
Tomek <t.durst@triadis.pl>
2019-06-05 18:48:51 +0200

    #82401 second approach to entity test

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -8,12 +8,19 @@ use App\DataFixtures\UserFixtures;
 use App\Entity\Department;
 use App\Entity\User;
 use Doctrine\Common\DataFixtures\ReferenceRepository;
+use Doctrine\Common\Persistence\Mapping\MappingException;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
 use Doctrine\ORM\Tools\SchemaTool;
 use Doctrine\ORM\Tools\ToolsException;
 use Liip\FunctionalTestBundle\Test\WebTestCase;
 
 
+/**
+ * @method entityManager(string $class)
+ */
 class UserTest extends WebTestCase
 {
     /**
@@ -63,11 +70,45 @@ class UserTest extends WebTestCase
         $this->entityManager = null;
     }
 
+    private function getEntityFromReference($referenceName): ?object
+    {
+        if (!$this->referenceRepository->hasReference($referenceName)) {
+            return null;
+        }
+
+        $reference = $this->referenceRepository->getReference($referenceName);
+
+        return $this->entityManager->getRepository(get_class($reference))->find($reference->getId());
+    }
+
     /**
-     * @throws \Doctrine\ORM\ORMException
-     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws ORMException
+     * @throws OptimisticLockException
      */
-    public function testSomething()
+    public function testUserAssignDepartment(): void
+    {
+        $user = $this->getEntityFromReference('user_admin');
+        /* @var $user User */
+        $this->assertInstanceOf(User::class, $user);
+
+        $department = $this->getEntityFromReference('department_1');
+        /* @var $department Department */
+        $this->assertInstanceOf(Department::class, $department);
+
+        $user->setDepartment($department);
+        $this->assertEquals($department, $user->getDepartment());
+
+        $user->setTitle('test');
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function testSomething(): void
     {
         $department = new Department();
         $department->setName('department 1');
@@ -75,10 +116,8 @@ class UserTest extends WebTestCase
         $department->setActive(true);
         $this->entityManager->persist($department);
 
-//        $this->referenceRepository->addReference('department_1', $department);
-
         $user = new User();
-        $user->setDepartment($department);
+        $user->setDepartment($this->getEntityFromReference('department_0'));
         $user->setSamAccountName('sam_account_name_1');
         $user->setUsername('user_name_1');
         $user->setEmail('user_email_1@example.com');
