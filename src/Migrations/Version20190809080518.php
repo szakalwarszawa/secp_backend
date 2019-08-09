4c75937016363f54279d1b025ba4f23696c66798
Konrad Szelepusta <konrad_szelepusta@parp.gov.pl>
2019-08-09 11:25:06 +0200

    linelength

--- src/Migrations/Version20190809080518.php
+++ src/Migrations/Version20190809080518.php
@@ -29,7 +29,10 @@ final class Version20190809080518 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'postgresql',
+            'Migration can only be executed safely on \'postgresql\'.'
+        );
 
         $this->addSql('CREATE SCHEMA logs');
         $this->addSql('CREATE SEQUENCE "logs"."ldap_import_log_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
