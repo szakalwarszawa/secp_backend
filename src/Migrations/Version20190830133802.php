9eb15e3e523d62c2503ff8dd25925d1c92b7a45f
hubertuspp <hubertuspp@gmail.com>
2019-08-30 18:59:35 +0200

    tracking changes in Timesheet, missing annotations in phpdoc

--- src/Migrations/Version20190830133802.php
+++ src/Migrations/Version20190830133802.php
@@ -12,19 +12,37 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20190830133802 extends AbstractMigration
 {
+    /**
+     * @return string
+     */
     public function getDescription() : string
     {
         return '';
     }
 
+    /**
+     * @param Schema $schema
+     * @throws \Doctrine\DBAL\DBALException
+     * @SuppressWarnings("unused")
+     */
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'postgresql',
+            'Migration can only be executed safely on \'postgresql\'.'
+        );
 
-        $this->addSql('ALTER INDEX idx_user_timesheets_user_timesheet_log_date RENAME TO idx_user_timesheet_log_user_timesheet_log_date');
+        $this->addSql(
+            'ALTER INDEX idx_user_timesheets_user_timesheet_log_date RENAME TO idx_user_timesheet_log_user_' .
+            'timesheet_log_date'
+        );
     }
 
+    /**
+     * @param Schema $schema
+     * @SuppressWarnings("unused")
+     */
     public function down(Schema $schema) : void
     {
         $this->abortIf(true, 'Downgrade migration can only be executed by next migration.');
