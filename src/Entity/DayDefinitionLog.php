067f207bb6bb7f04143d5c0ae1511bc96d0a08b2
Konrad Szelepusta <konrad.szelepusta@parp.gov.pl>
2019-10-26 15:09:56 +0200

    naprawia #93451

--- src/Entity/DayDefinitionLog.php
+++ src/Entity/DayDefinitionLog.php
@@ -1,13 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
-use App\Entity\Types\LogEntityInterface;
-use DateTimeInterface;
+use App\Annotations\ParentEntity;
+use App\Entity\Types\Model\LogEntity;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Serializer\Annotation\Groups;
-use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Table(
@@ -15,7 +15,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  *     name="`day_definition_logs`",
  *     indexes={
  *          @ORM\Index(name="idx_day_definitions_log_date", columns={"log_date"}),
- *          @ORM\Index(name="idx_day_definitions_day_definition_log_date", columns={"day_definition_id", "log_date"}),
+ *          @ORM\Index(name="idx_day_definitions_day_definition_log_date", columns={"parent_id", "log_date"}),
  *     }
  * )
  * @ORM\Entity(repositoryClass="App\Repository\DayDefinitionLogRepository")
@@ -42,168 +42,8 @@ use Symfony\Component\Validator\Constraints as Assert;
  *          }
  *      }
  * )
+ * @ParentEntity(DayDefinition::class)
  */
-class DayDefinitionLog /*implements LogEntityInterface*/
+class DayDefinitionLog extends LogEntity
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     * @Groups({"get"})
-     */
-    private $id;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\DayDefinition", inversedBy="dayDefinitionLogs")
-     * @ORM\JoinColumn(nullable=false, columnDefinition="CHAR(10) NOT NULL")
-     * @Groups({"get"})
-     */
-    private $dayDefinition;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\User")
-     * @ORM\JoinColumn(nullable=false)
-     * @Groups({"get"})
-     */
-    private $owner;
-
-    /**
-     * @var DateTimeInterface
-     *
-     * @ORM\Column(type="datetime")
-     * @Assert\NotBlank()
-     * @Groups({"get"})
-     */
-    private $logDate;
-
-    /**
-     * @ORM\Column(type="text")
-     * @Assert\NotBlank()
-     * @Groups({"get"})
-     */
-    private $notice;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(
-     *  type="string",
-     *  length=100,
-     *  nullable=true
-     * )
-     * @Groups({"get"})
-     */
-    private $trigger;
-
-    /**
-     * @return int|null
-     */
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    /**
-     * @return DayDefinition|null
-     */
-    public function getDayDefinition(): ?DayDefinition
-    {
-        return $this->dayDefinition;
-    }
-
-    /**
-     * @param DayDefinition|null $dayDefinition
-     *
-     * @return DayDefinitionLog
-     */
-    public function setDayDefinition(?DayDefinition $dayDefinition): DayDefinitionLog
-    {
-        $this->dayDefinition = $dayDefinition;
-
-        return $this;
-    }
-
-    /**
-     * @return User|null
-     */
-    public function getOwner(): ?User
-    {
-        return $this->owner;
-    }
-
-    /**
-     * @param User|null $owner
-     *
-     * @return DayDefinitionLog
-     */
-    public function setOwner(?User $owner): DayDefinitionLog
-    {
-        $this->owner = $owner;
-
-        return $this;
-    }
-
-    /**
-     * @return DateTimeInterface
-     */
-    public function getLogDate(): DateTimeInterface
-    {
-        return $this->logDate;
-    }
-
-    /**
-     * @param DateTimeInterface $logDate
-     *
-     * @return DayDefinitionLog
-     */
-    public function setLogDate(DateTimeInterface $logDate): DayDefinitionLog
-    {
-        $this->logDate = $logDate;
-
-        return $this;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getNotice(): ?string
-    {
-        return $this->notice;
-    }
-
-    /**
-     * @param string $notice
-     *
-     * @return DayDefinitionLog
-     */
-    public function setNotice(string $notice): DayDefinitionLog
-    {
-        $this->notice = $notice;
-
-        return $this;
-    }
-
-    /**
-     * Get trigger
-     *
-     * @return string|null
-     */
-    public function getTrigger(): ?string
-    {
-        return $this->trigger;
-    }
-
-    /**
-     * Set trigger
-     *
-     * @param string|null $trigger
-     *
-     * @return DayDefinitionLog
-     */
-    public function setTrigger(?string $trigger): DayDefinitionLog
-    {
-        $this->trigger = $trigger;
-
-        return $this;
-    }
 }
