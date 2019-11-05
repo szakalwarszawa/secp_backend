603a84d7774f6f4f260cbdec99956b2e625ab9f3
Konrad Szelepusta <konrad_szelepusta@parp.gov.pl>
2019-11-05 08:53:47 +0100

    user fix

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -413,6 +413,13 @@ class User implements UserInterface, LoggableEntityInterface
     private $userTimesheets;
 
     /**
+     * @var string
+     *
+     * @Groups({"get", "put", "post"})
+     */
+    private $fullName = '';
+
+    /**
      * User constructor.
      */
     public function __construct()
@@ -942,5 +949,13 @@ class User implements UserInterface, LoggableEntityInterface
         }
 
         return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFullName(): string
+    {
+        return $this->firstName . ' ' . $this->lastName;
     }
 }
